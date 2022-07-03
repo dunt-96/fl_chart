3fc8e9eaf416e41bb79ccb4493196300f7c2784e
DuNT <ntd160296@gmail.com>
2022-07-03 15:21:25 +0700

    add barchart

---
+++ lib/src/chart/bar_chart/barchart_linechart.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LineBarChart extends StatefulWidget {
+  const LineBarChart({Key? key}) : super(key: key);
+
+  @override
+  State<LineBarChart> createState() => _LineBarChartState();
+}
+
+class _LineBarChartState extends State<LineBarChart> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
